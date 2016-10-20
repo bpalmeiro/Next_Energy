b53310defa491be5e18c870fcb0dff1bf2b7a842
Brais <brais.pp@gmail.com>
2016-10-20 15:22:35 +0200

    Histogram class fixing

--- Tools/Histogram.py
+++ Tools/Histogram.py
@@ -1,3 +1,4 @@
+# %load ../Tools/Histogram.py
 import numpy as np
 
 
@@ -5,80 +6,77 @@ class Histogram():
     '''
     Class to storage histograms
     '''
-    def __init__ (self,data=[], nbin = None, minlim = None, maxlim = None):
+    def __init__(self, data=[], nbin=None, minlim=None, maxlim=None):
         '''
-        Creates an histogram from a given a list (empty by defoult), 
-        with low and high limits, by defoult are the minimum and maximum 
+        Creates an histogram from a given a list (empty by defoult),
+        with low and high limits, by defoult are the minimum and maximum
         of de data sample and the bin number, by default the number of
         data squareroot
-        
-        '''  
-        
-        N = len(data)
-        if N == 0:
-            if minlim=='nada' or maxlim=='nada' or nbin=='nada':
+        '''
+
+        if len(data) == 0:
+            if minlim == 'nada' or maxlim == 'nada' or nbin == 'nada':
                 auxs = 'In order to build an empty histogram it '
                 auxs += 'is needed a minlim, maxlim and nbin'
-                    
-                print auxs
+                raise ValueError(auxs)
                 return
-            else:   
+            else:
                 self.minlim = minlim
                 self.maxlim = maxlim
                 self.nbin = nbin
-            
+
         else:
-            if minlim==None:
+            if minlim is None:
                 self.minlim = min(data)
             else:
-                self.minlim =minlim
-            if maxlim==None:
+                self.minlim = minlim
+            if maxlim is None:
                 self.maxlim = max(data)
             else:
-                self.maxlim = maxlim 
-            if nbin==None:
-                self.nbin = int(N**0.5)
+                self.maxlim = maxlim
+            if nbin is None:
+                self.nbin = int(len(data)**0.5)
             else:
                 self.nbin = nbin
 
         self.binsize = float(self.maxlim-self.minlim)/float(self.nbin)
         self.hist = np.array([])
         self.bins = np.array([])
-        
+
         self.Build_hist(data)
 
-    def __add__(self,hist):
+    def __add__(self, hist):
         '''
         Suming histograms
-        '''  
-        
-        if not self.minlim==hist.minlim and self.maxlim==hist.maxlim and self.nbin==hist.nbin:
-            raise InputError('Histograms no compatible')
+        '''
+
+        if not self.minlim == hist.minlim or not self.maxlim == hist.maxlim\
+                or not self.nbin == hist.nbin:
+            raise ValueError('Histograms are not compatible')
         else:
-            auxhist = Histogram([],self.minlim, self.maxlim, self.nbin)
+            auxhist = Histogram([], self.nbin, self.minlim, self.maxlim)
             auxhist.hist = self.hist + hist.hist
         return auxhist
-        
-        
+
     def Build_hist(self, data):
         '''
         Creating histogram using numpy ones but with bin centers instead
         '''
-        auxhist,auxbins = np.histogram(np.array(data),self.nbin,[self.minlim,self.maxlim])
+        auxhist, auxbins = np.histogram(np.array(data), self.nbin,
+                                        [self.minlim, self.maxlim])
         auxbins = auxbins[:-1] + np.diff(auxbins)/2.
         self.hist = auxhist
         self.bins = auxbins
-     
+
     def Fill_hist(self, data):
         '''
         Filling histogram
         '''
         data = np.array(data)
-        self.hist += np.histogram(np.array(data),self.nbin,[self.minlim,self.maxlim])[0]
-    
-    
-    
-    def Scale(self,factor):
+        self.hist += np.histogram(np.array(data), self.nbin, [self.minlim,
+                                                              self.maxlim])[0]
+
+    def Scale(self, factor):
         '''
         Explicit __rmul__, scales the histogram by a factor
         '''
